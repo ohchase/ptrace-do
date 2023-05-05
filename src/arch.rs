67d25c99b75a295a1ee4e0c7934062c260d7677e
vap0rwaves <25832164+vap0rwaves@users.noreply.github.com>
2023-05-05 14:38:24 -0400

    continue

---
+++ src/arch.rs
@@ -0,0 +1,194 @@
+#[cfg(target_arch = "x86")]
+pub mod x86 {
+
+    #[repr(C)]
+    #[derive(Debug, Clone)]
+    pub struct UserRegs {
+        pub ebx: i32,
+        pub ecx: i32,
+        pub edx: i32,
+        pub esi: i32,
+        pub edi: i32,
+        pub ebp: i32,
+        pub eax: i32,
+        pub xds: i32,
+        pub xes: i32,
+        pub xfs: i32,
+        pub xgs: i32,
+        pub orig_eax: i32,
+        pub eip: i32,
+        pub xcs: i32,
+        pub eflags: i32,
+        pub esp: i32,
+        pub xss: i32,
+    }
+}
+
+#[cfg(target_arch = "x86_64")]
+pub mod x86_64 {
+    #[repr(C)]
+    #[derive(Debug, Clone)]
+    pub struct UserRegs {
+        pub r15: u64,
+        pub r14: u64,
+        pub r13: u64,
+        pub r12: u64,
+        pub rbp: u64,
+        pub rbx: u64,
+        pub r11: u64,
+        pub r10: u64,
+        pub r9: u64,
+        pub r8: u64,
+        pub rax: u64,
+        pub rcx: u64,
+        pub rdx: u64,
+        pub rsi: u64,
+        pub rdi: u64,
+        pub orig_rax: u64,
+        pub rip: u64,
+        pub cs: u64,
+        pub eflags: u64,
+        pub rsp: u64,
+        pub ss: u64,
+        pub fs_base: u64,
+        pub gs_base: u64,
+        pub ds: u64,
+        pub es: u64,
+        pub fs: u64,
+        pub gs: u64,
+    }
+
+    impl UserRegs {
+        pub fn program_counter(&self) -> usize {
+            self.rip as usize
+        }
+
+        pub fn set_program_counter(&mut self, program_counter: usize) {
+            self.rip = program_counter as u64
+        }
+
+        pub fn stack_pointer(&self) -> usize {
+            self.rsp as usize
+        }
+
+        pub fn set_stack_pointer(&mut self, stack_pointer: usize) {
+            self.rsp = stack_pointer as u64
+        }
+    }
+}
+
+#[cfg(target_arch = "arm")]
+pub mod arm {
+
+    pub const CPSR_T_MASK: u32 = 1 << 5;
+
+    /// Note: Entries 0-15 match r0..r15
+    /// Entry 16 is used to store the CPSR register.
+    /// Entry 17 is used to store the "orig_r0" value.
+    #[repr(C)]
+    #[derive(Debug, Clone)]
+    pub struct UserRegs {
+        pub regs: [u32; 18],
+    }
+
+    impl UserRegs {
+        pub fn return_value(&self) -> usize {
+            self.regs[0] as usize
+        }
+
+        pub fn stack_pointer(&self) -> usize {
+            self.regs[13] as usize
+        }
+
+        pub fn set_stack_pointer(&mut self, stack_pointer: usize) {
+            self.regs[13] = stack_pointer as u64
+        }
+
+        pub fn lr(&self) -> usize {
+            self.regs[14] as usize
+        }
+
+        pub fn set_lr(&mut self, lr: usize) {
+            self.regs[14] = lr as u64
+        }
+
+        pub fn program_counter(&self) -> usize {
+            self.regs[15] as usize
+        }
+
+        pub fn set_program_counter(&mut self, program_counter: usize) {
+            self.regs[15] = program_counter as u64
+        }
+
+        pub fn cpsr(&self) -> usize {
+            self.regs[16] as usize
+        }
+
+        pub fn set_cpsr(&mut self, cpsr: usize) {
+            self.regs[16] = cpsr as u64
+        }
+    }
+}
+
+#[cfg(target_arch = "aarch64")]
+pub mod aarch64 {
+
+    #[repr(C)]
+    #[derive(Debug, Clone)]
+    pub struct UserRegs {
+        pub regs: [u64; 31],
+        pub sp: u64,
+        pub pc: u64,
+        pub pstate: u64,
+    }
+
+    impl UserRegs {
+        pub fn return_value(&self) -> usize {
+            self.regs[0] as usize
+        }
+
+        pub fn stack_pointer(&self) -> usize {
+            self.sp as usize
+        }
+
+        pub fn set_stack_pointer(&mut self, stack_pointer: usize) {
+            self.sp = stack_pointer as u64
+        }
+
+        pub fn lr(&self) -> usize {
+            self.regs[30] as usize
+        }
+
+        pub fn set_lr(&mut self, lr: usize) {
+            self.regs[30] = lr as u64
+        }
+
+        pub fn program_counter(&self) -> usize {
+            self.pc as usize
+        }
+
+        pub fn set_program_counter(&mut self, program_counter: usize) {
+            self.pc = program_counter as u64
+        }
+
+        pub fn cpsr(&self) -> usize {
+            self.pstate as usize
+        }
+
+        pub fn set_cpsr(&mut self, cpsr: usize) {
+            self.pstate = cpsr as u64
+        }
+    }
+}
+
+#[cfg(target_arch = "aarch64")]
+pub use aarch64::*;
+
+#[cfg(target_arch = "arm")]
+pub use arm::*;
+
+#[cfg(target_arch = "x86_64")]
+pub use x86_64::*;
+
+#[cfg(target_arch = "x86")]
+pub use x86::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,545 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod arch;
+use arch::UserRegs;
+
+use libc::{pid_t, ptrace, PTRACE_ATTACH, PTRACE_CONT, PTRACE_DETACH};
+use proc_maps::MapRange;
+use std::{mem, process::Child};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum TraceError {
+    #[error("Ptrace error: `{0}`")]
+    Ptrace(std::io::Error),
+
+    #[error("IO Error: `{0}`")]
+    Io(#[from] std::io::Error),
+
+    #[error("General `{0}`")]
+    General(&'static str),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+type TraceResult<T> = Result<T, TraceError>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub trait ProcessIdentifier {
+    fn pid(&self) -> pid_t;
+}
+
+pub struct RawProcess {
+    pid: pid_t,
+}
+
+impl ProcessIdentifier for RawProcess {
+    fn pid(&self) -> pid_t {
+        self.pid
+    }
+}
+
+pub struct OwnedProcess {
+    child: Child,
+}
+
+impl OwnedProcess {
+    pub fn of_child(child: Child) -> Self {
+        Self { child }
+    }
+}
+
+impl Drop for OwnedProcess {
+    fn drop(&mut self) {
+        if let Err(e) = self.child.kill() {
+            tracing::error!("Unable to kill owned process's child {e:?}");
+        }
+    }
+}
+
+impl ProcessIdentifier for OwnedProcess {
+    fn pid(&self) -> pid_t {
+        self.child.id() as pid_t
+    }
+}
+
+pub struct TracedProcess<T>
+where
+    T: ProcessIdentifier,
+{
+    process: T,
+}
+
+#[allow(unused)]
+enum WaitOptions {
+    None,
+    NoHang,
+    Untraced,
+    Continued,
+}
+
+impl From<WaitOptions> for i32 {
+    fn from(val: WaitOptions) -> Self {
+        match val {
+            WaitOptions::None => 0,
+            WaitOptions::NoHang => libc::WNOHANG,
+            WaitOptions::Untraced => libc::WUNTRACED,
+            WaitOptions::Continued => libc::WCONTINUED,
+        }
+    }
+}
+
+struct WaitStatus(i32);
+
+impl WaitStatus {
+    fn is_stop(&self) -> bool {
+        libc::WIFSTOPPED(self.0)
+    }
+
+    fn is_signaled(&self) -> bool {
+        libc::WIFSIGNALED(self.0)
+    }
+
+    fn is_continued(&self) -> bool {
+        libc::WIFCONTINUED(self.0)
+    }
+
+    fn is_exited(&self) -> bool {
+        libc::WIFEXITED(self.0)
+    }
+
+    fn stop_code(&self) -> i32 {
+        libc::WSTOPSIG(self.0)
+    }
+}
+
+impl std::fmt::Debug for WaitStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("WaitStatus")
+            .field("is_stopped", &self.is_stop())
+            .field("is_signaled", &self.is_signaled())
+            .field("is_continued", &self.is_continued())
+            .field("is_exited", &self.is_exited())
+            .field("stop_code", &self.stop_code())
+            .finish()
+    }
+}
+
+pub struct ProcessFrame<T>
+where
+    T: ProcessIdentifier,
+{
+    process: TracedProcess<T>,
+}
+
+impl<T> std::fmt::Debug for ProcessFrame<T>
+where
+    T: ProcessIdentifier,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ProcessFrame ( pid: {} )", self.process.pid())
+    }
+}
+
+impl<T> ProcessFrame<T>
+where
+    T: ProcessIdentifier,
+{
+    #[cfg(target_arch = "aarch64")]
+    pub fn query_registers(&mut self) -> TraceResult<UserRegs> {
+        let mut registers: UserRegs = unsafe { mem::zeroed() };
+        let mut iovec = libc::iovec {
+            iov_base: &mut registers as *mut _ as *mut core::ffi::c_void,
+            iov_len: std::mem::size_of::<UserRegs>() as libc::size_t,
+        };
+
+        let result = unsafe { ptrace(libc::PTRACE_GETREGSET, self.process.pid(), 1, &mut iovec) };
+
+        match result == -1 {
+            true => Err(TraceError::Ptrace(std::io::Error::last_os_error())),
+            false => Ok(registers),
+        }
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    pub fn set_registers(&mut self, mut registers: UserRegs) -> TraceResult<()> {
+        let mut iovec = libc::iovec {
+            iov_base: &mut registers as *mut _ as *mut core::ffi::c_void,
+            iov_len: std::mem::size_of::<UserRegs>() as libc::size_t,
+        };
+
+        let result = unsafe { ptrace(libc::PTRACE_SETREGSET, self.process.pid(), 1, &mut iovec) };
+
+        match result == -1 {
+            true => Err(TraceError::Ptrace(std::io::Error::last_os_error())),
+            false => Ok(()),
+        }
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    pub fn invoke_remote(
+        mut self,
+        func_address: usize,
+        return_address: usize,
+        parameters: &[usize],
+    ) -> TraceResult<(UserRegs, ProcessFrame<T>)> {
+        use std::mem::size_of;
+
+        const REGISTER_ARGUMENTS: usize = 8;
+        let mut current_registers = self.query_registers()?;
+        tracing::info!(
+            "Initial registers acquired Current PC: {:X?}",
+            current_registers.program_counter()
+        );
+
+        let cached_registers = current_registers.clone();
+        for (i, param) in parameters[..std::cmp::min(parameters.len(), REGISTER_ARGUMENTS)]
+            .iter()
+            .enumerate()
+        {
+            let reg: usize = *param;
+            current_registers.regs[i] = reg;
+            tracing::info!("Applying register {i} with param {}", reg);
+        }
+
+        if parameters.len() > REGISTER_ARGUMENTS {
+            let stack_arguments = &parameters[REGISTER_ARGUMENTS..];
+            tracing::info!("Remaining stack arguments: {:?}", stack_arguments);
+
+            // adjust stack pointer
+            current_registers.set_stack_pointer(
+                current_registers.stack_pointer() - (stack_arguments.len() * size_of::<usize>()),
+            );
+
+            self.write_memory(
+                current_registers.stack_pointer(),
+                bytemuck::cast_slice(stack_arguments),
+            )?;
+        };
+
+        // set registers cached_registers
+        current_registers.set_program_counter(func_address);
+        if (current_registers.program_counter() & 1) != 0 {
+            current_registers.set_program_counter(current_registers.program_counter() & !1);
+            current_registers.set_cpsr(current_registers.cpsr() | (1 << 5));
+        } else {
+            current_registers.set_cpsr(current_registers.cpsr() & !(1 << 5));
+        }
+        current_registers.set_lr(return_address);
+        tracing::trace!(
+            "Executing with PC: {:X?}, and arguments {parameters:?}",
+            func_address
+        );
+
+        self.set_registers(current_registers)?;
+        tracing::trace!("Registers successfully injected.");
+
+        let mut frame = self.step_cont()?;
+        let result_regs = frame.query_registers()?;
+        tracing::trace!("Result {result_regs:#?}");
+
+        frame.set_registers(cached_registers)?;
+        Ok((result_regs, frame))
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64", target_arch = "arm"))]
+    pub fn query_registers(&mut self) -> TraceResult<UserRegs> {
+        let mut registers: UserRegs = unsafe { mem::zeroed() };
+        let result = unsafe { ptrace(libc::PTRACE_GETREGS, self.process.pid(), 0, &mut registers) };
+
+        match result == -1 {
+            true => Err(TraceError::Ptrace(std::io::Error::last_os_error())),
+            false => Ok(registers),
+        }
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64", target_arch = "arm"))]
+    pub fn set_registers(&mut self, registers: UserRegs) -> TraceResult<()> {
+        let result = unsafe { ptrace(libc::PTRACE_SETREGS, self.process.pid(), 0, &registers) };
+
+        match result == -1 {
+            true => Err(TraceError::Ptrace(std::io::Error::last_os_error())),
+            false => Ok(()),
+        }
+    }
+
+    #[cfg(target_arch = "arm")]
+    pub fn invoke_remote(
+        mut self,
+        func_address: usize,
+        return_address: usize,
+        parameters: &[u32],
+    ) -> TraceResult<(UserRegs, ProcessFrame<T>)> {
+        use std::mem::size_of;
+
+        const REGISTER_ARGUMENTS: usize = 4;
+        let mut current_registers = self.query_registers()?;
+        tracing::info!(
+            "Initial registers acquired Current PC: {:X?}",
+            current_registers.program_counter()
+        );
+
+        let cached_registers = current_registers.clone();
+        for (i, param) in parameters[..std::cmp::min(parameters.len(), REGISTER_ARGUMENTS)]
+            .iter()
+            .enumerate()
+        {
+            let reg: u32 = (*param).into();
+            current_registers.regs[i] = reg;
+            tracing::info!("Applying register {i} with param {}", reg);
+        }
+
+        if parameters.len() > REGISTER_ARGUMENTS {
+            let stack_arguments = &parameters[REGISTER_ARGUMENTS..];
+            tracing::info!("Remaining stack arguments: {:?}", stack_arguments);
+
+            // adjust stack pointer
+            current_registers.set_stack_pointer(
+                current_registers.stack_pointer()
+                    - (stack_arguments.len() * size_of::<usize>()) as u32,
+            );
+
+            self.write_memory(
+                current_registers.stack_pointer() as usize,
+                bytemuck::cast_slice(stack_arguments),
+            )?;
+        };
+
+        // set registers cached_registers
+        current_registers.set_program_counter(func_address as u32);
+        if (current_registers.program_counter() & 1) != 0 {
+            current_registers.set_program_counter(current_registers.program_counter() & !1);
+            current_registers.set_cpsr(current_registers.cpsr() | (1 << 5));
+        } else {
+            current_registers.set_cpsr(current_registers.cpsr() & !(1 << 5));
+        }
+        current_registers.set_lr(return_address as u32);
+        tracing::trace!(
+            "Executing with PC: {:X?}, and arguments {parameters:?}",
+            func_address
+        );
+
+        self.set_registers(current_registers)?;
+        tracing::trace!("Registers successfully injected.");
+
+        let mut frame = self.step_cont()?;
+        let result_regs = frame.query_registers()?;
+        tracing::trace!("Result {result_regs:#?}");
+
+        frame.set_registers(cached_registers)?;
+        Ok((result_regs, frame))
+    }
+
+    #[cfg(target_arch = "x86_64")]
+    pub fn invoke_remote(
+        mut self,
+        func_address: usize,
+        parameters: &[usize],
+    ) -> TraceResult<(UserRegs, ProcessFrame<T>)> {
+        use std::mem::size_of;
+
+        const REGISTER_ARGUMENTS: usize = 6;
+        let mut current_registers = self.query_registers()?;
+        tracing::info!(
+            "Initial registers acquired Current PC: {:X?}",
+            current_registers.program_counter()
+        );
+
+        let cached_registers = current_registers.clone();
+        let param_count = parameters.len();
+
+        // You gotta a better idea???????
+        if param_count > 0 {
+            current_registers.rdi = parameters[0] as u64;
+        }
+        if param_count > 1 {
+            current_registers.rsi = parameters[1] as u64;
+        }
+        if param_count > 2 {
+            current_registers.rdx = parameters[2] as u64;
+        }
+        if param_count > 3 {
+            current_registers.rcx = parameters[3] as u64;
+        }
+        if param_count > 4 {
+            current_registers.r8 = parameters[4] as u64;
+        }
+        if param_count > 5 {
+            current_registers.r9 = parameters[5] as u64;
+        }
+
+        if parameters.len() > REGISTER_ARGUMENTS {
+            let stack_arguments = &parameters[REGISTER_ARGUMENTS..];
+            tracing::info!("Remaining stack arguments: {:?}", stack_arguments);
+
+            // adjust stack pointer
+            current_registers.set_stack_pointer(
+                current_registers.stack_pointer() - (stack_arguments.len() * size_of::<usize>()),
+            );
+
+            self.write_memory(current_registers.stack_pointer(), unsafe {
+                std::mem::transmute(stack_arguments)
+            })?;
+        };
+
+        // set registers cached_registers
+        current_registers.set_program_counter(func_address);
+        tracing::trace!(
+            "Executing with PC: {:X?}, and arguments {parameters:?}",
+            func_address
+        );
+
+        self.set_registers(current_registers)?;
+        tracing::trace!("Registers successfully injected.");
+
+        let mut frame = self.step_cont()?;
+        let result_regs = frame.query_registers()?;
+        tracing::trace!("Result {result_regs:#?}");
+
+        frame.set_registers(cached_registers)?;
+        Ok((result_regs, frame))
+    }
+
+    pub fn read_memory(&mut self, addr: usize, len: usize) -> TraceResult<Vec<u8>> {
+        let mut data = vec![0; len];
+        let len_read = self.read_memory_mut(addr, &mut data)?;
+        data.truncate(len_read);
+        Ok(data)
+    }
+
+    pub fn read_memory_mut(&self, addr: usize, data: &mut [u8]) -> TraceResult<usize> {
+        use std::os::unix::fs::FileExt;
+        let mem = std::fs::File::open(self.process.proc_mem_path())?;
+        let len = mem.read_at(data, addr.try_into().unwrap())?;
+        Ok(len)
+    }
+
+    pub fn write_memory(&mut self, addr: usize, data: &[u8]) -> TraceResult<usize> {
+        use std::os::unix::fs::FileExt;
+        let mem = std::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .open(self.process.proc_mem_path())?;
+        let len = mem.write_at(data, addr.try_into().unwrap())?;
+        Ok(len)
+    }
+
+    fn step_cont(mut self) -> TraceResult<ProcessFrame<T>> {
+        self.process.cont()?;
+        self.process.next_frame()
+    }
+}
+
+fn find_module_map_internal(mod_name: &str) -> TraceResult<Option<MapRange>> {
+    use proc_maps::get_process_maps;
+
+    let maps = get_process_maps(std::process::id() as libc::pid_t)?;
+    Ok(maps.into_iter().find(|m| match m.filename() {
+        Some(p) => p.to_str().expect("str") == mod_name,
+        None => false,
+    }))
+}
+
+impl<T> Drop for TracedProcess<T>
+where
+    T: ProcessIdentifier,
+{
+    fn drop(&mut self) {
+        let pid = self.pid();
+        match self.detach() {
+            Ok(()) => tracing::info!("Successfully detached from Pid: {pid}"),
+            Err(e) => tracing::error!("Failed to detach from Pid: {pid}, {e:#?}"),
+        }
+    }
+}
+
+impl<T> TracedProcess<T>
+where
+    T: ProcessIdentifier,
+{
+    fn detach(&mut self) -> TraceResult<()> {
+        let result = unsafe { ptrace(PTRACE_DETACH, self.pid(), 0, 0) };
+        match result == -1 {
+            true => Err(TraceError::Ptrace(std::io::Error::last_os_error())),
+            false => Ok(()),
+        }
+    }
+
+    pub fn attach(process: T) -> TraceResult<Self> {
+        let result = unsafe { ptrace(PTRACE_ATTACH, process.pid(), 0, 0) };
+        match result == -1 {
+            true => Err(TraceError::Ptrace(std::io::Error::last_os_error())),
+            false => Ok(Self { process }),
+        }
+    }
+
+    fn find_module_map(&self, mod_name: &str) -> TraceResult<Option<MapRange>> {
+        use proc_maps::get_process_maps;
+
+        let maps = get_process_maps(self.process.pid())?;
+        Ok(maps.into_iter().find(|m| match m.filename() {
+            Some(p) => p.to_str().expect("str") == mod_name,
+            None => false,
+        }))
+    }
+
+    pub fn find_remote_procedure(
+        &self,
+        mod_name: &str,
+        function_address: usize,
+    ) -> TraceResult<Option<usize>> {
+        let Some(remote_module) = self.find_module_map(mod_name)? else {
+            tracing::warn!("Unable to identify {mod_name:?} externally");
+            return Ok(None);
+        };
+        tracing::info!(
+            "Identifed remote range {mod_name:?} at {:X?}",
+            remote_module.start()
+        );
+
+        let Some(internal_module) = find_module_map_internal(mod_name)? else {
+            tracing::warn!("Unable to identify {mod_name:?} internally");
+            return Ok(None);
+        };
+        tracing::info!(
+            "Identifed internal range {mod_name:?} at {:X?}",
+            internal_module.start()
+        );
+
+        Ok(Some(
+            function_address - internal_module.start() + remote_module.start(),
+        ))
+    }
+
+    pub fn pid(&self) -> pid_t {
+        self.process.pid()
+    }
+
+    fn cont(&mut self) -> TraceResult<()> {
+        let result = unsafe { ptrace(PTRACE_CONT, self.pid(), 0, 0) };
+        match result == -1 {
+            true => Err(TraceError::Ptrace(std::io::Error::last_os_error())),
+            false => Ok(()),
+        }
+    }
+
+    fn wait(&mut self, options: WaitOptions) -> TraceResult<WaitStatus> {
+        let mut raw_status = 0;
+        let result = unsafe { libc::waitpid(self.process.pid(), &mut raw_status, options.into()) };
+        if result == -1 {
+            return Err(TraceError::Ptrace(std::io::Error::last_os_error()));
+        }
+
+        let status = WaitStatus(raw_status);
+        tracing::info!("{status:?}");
+        Ok(status)
+    }
+
+    pub fn next_frame(mut self) -> TraceResult<ProcessFrame<T>> {
+        let wait_status = self.wait(WaitOptions::Untraced)?;
+        if wait_status.is_exited() {
+            return Err(TraceError::General("Waiting stop received an exit signal"));
+        }
+
+        Ok(ProcessFrame { process: self })
+    }
+
+    fn proc_mem_path(&self) -> String {
+        format!("/proc/{}/mem", self.process.pid())
     }
 }
