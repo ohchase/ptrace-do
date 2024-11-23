3ae9d7bdbde2a53e458ce3dd710d6acd56a59ed5
ohchase <ohchase@proton.me>
2024-11-23 09:36:18 -0500

    clean up, and document example better

--- examples/get_pid_explicit.rs
+++ examples/get_pid_explicit.rs
@@ -3,29 +3,34 @@ use libc::pid_t;
 use proc_maps::MapRange;
 use ptrace_do::{ProcessIdentifier, RawProcess, TracedProcess};
 
-fn find_mod_map_fuzzy(mod_name: &str, process: &impl ProcessIdentifier) -> Option<MapRange> {
+/// Just searches the proc maps for a module ending with the provided mod_name
+fn find_mod_map_fuzzy(mod_name: &str, pid: pid_t) -> Option<MapRange> {
     use proc_maps::get_process_maps;
-    let maps = get_process_maps(process.pid()).expect("alive");
+    let maps = get_process_maps(pid).expect("alive");
     maps.into_iter().find(|m| match m.filename() {
         Some(p) => p.to_str().map(|s| s.contains(mod_name)).unwrap_or(false),
         None => false,
     })
 }
 
-pub fn find_remote_procedure(
+/// This `finds` the remote procedure's virtual offsets.
+/// This checks for the target library in our own process and sees how far into the lib the function was mapped for us.
+/// We can then apply this offset to the traced process's mapping of the same library.
+/// Thus we end up at the target process's function
+fn find_remote_procedure<P: ProcessIdentifier>(
     mod_name: &str,
-    owned_process: &impl ProcessIdentifier,
-    remote_process: &impl ProcessIdentifier,
+    owned_process: &P,
+    remote_process: &TracedProcess<P>,
     function_address: usize,
 ) -> Option<usize> {
-    let internal_module = find_mod_map_fuzzy(mod_name, owned_process)?;
+    let internal_module = find_mod_map_fuzzy(mod_name, owned_process.pid())?;
     tracing::info!(
         "Identifed internal range {mod_name:?} ({:?}) at {:X?}",
         internal_module.filename(),
         internal_module.start()
     );
 
-    let remote_module = find_mod_map_fuzzy(mod_name, remote_process)?;
+    let remote_module = find_mod_map_fuzzy(mod_name, remote_process.pid())?;
     tracing::info!(
         "Identifed remote range {mod_name:?} ({:?}) at {:X?}",
         remote_module.filename(),
@@ -38,6 +43,8 @@ pub fn find_remote_procedure(
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt().init();
 
+    // start your target process and figure out its pid.
+    // ps -A | grep my_cool_bin .... etc
     let target_pid: pid_t = 7777;
     let traced_process = TracedProcess::attach(RawProcess::new(target_pid))?;
 
