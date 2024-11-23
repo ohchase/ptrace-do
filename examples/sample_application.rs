f54e1549210969f7fdb64ffb014318ee9dfa01de
ohchase <ohchase@proton.me>
2024-11-23 09:17:59 -0500

    clean up, documentation, examples minimize

--- examples/sample_application.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("Hello my pid is: {}", unsafe { libc::getpid() });
-
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(3));
-        println!("Tick.");
-    }
-}
